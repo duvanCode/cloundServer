eea23ed8cca2275a5a11a3e7467ad9503a346095
duvanCode <52578979+duvanCode@users.noreply.github.com>
2025-08-04 13:04:30 -0500

    Delete app/routes/home.routes.js

--- app/routes/home.routes.js
+++
@@ -1,9 +0,0 @@
-const homeRoute = async (req, res) => {
-    res.status(200).json({
-        "success": true,
-        "message": 'Todo bien, todo correcto y yo que me alegroo. 😎',
-        "data": null
-    });
-}
-
-module.exports = homeRoute;

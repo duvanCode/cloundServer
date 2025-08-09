c0a07d6439c1827df9f55e045367c0e9be70592b
duvanCode <52578979+duvanCode@users.noreply.github.com>
2025-08-08 22:52:43 -0500

    Update app.js

--- app.js
+++ app.js
@@ -9,3 +9,7 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerJsdoc(options)));
 app.listen(process.env.PORT, () => {
     console.log("servidor iniciado en el puerto:" + process.env.PORT);
   });
+
+app.timeout = parseInt(SERVER_TIMEOUT);
+app.keepAliveTimeout = parseInt(SERVER_TIMEOUT);
+app.headersTimeout = parseInt(SERVER_TIMEOUT) + 1000;

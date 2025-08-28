7c8c7ebb065e6a16209f67b2db6cf723ec374a93
duvanCode <52578979+duvanCode@users.noreply.github.com>
2025-08-28 15:18:14 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,8 @@
 require('dotenv').config();
 const app = require('./app/routes/file.routes');
-const swaggerUi = require('swagger-ui-express');
-const swaggerJsdoc = require('swagger-jsdoc');
-const options = require('./swagger.js');
 
 const SERVER_TIMEOUT = process.env.SERVER_TIMEOUT || (10 * 60 * 1000);
 const PORT = process.env.PORT;
-
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerJsdoc(options)));
 
 app.listen(PORT, () => {
     console.log("servidor iniciado en el puerto:" + process.env.PORT);

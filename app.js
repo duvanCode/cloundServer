f48b7cd9266bc3e38af3024f072591807db36cd3
DuvanCode <duvanyepezfa@gmail.com>
2025-09-01 15:19:15 -0500

    add routes by controllers

--- app.js
+++ app.js
@@ -1,8 +1,35 @@
 require('dotenv').config();
-const app = require('./app/routes/file.routes');
+const express = require('express');
+const contentSecurityPolicy = require('./app/middlewares/contentSecurityPolicy.middleware.js');
+const cors = require('cors');
 
-const SERVER_TIMEOUT = process.env.SERVER_TIMEOUT || (10 * 60 * 1000);
+const homeRoutes = require('./app/routes/home.routes');
+const registerRoutes = require('./app/routes/register.routes');
+const fileRoutes = require('./app/routes/file.routes');
+
+const app = express();
+
+const SERVER_TIMEOUT = process.env.SERVER_TIMEOUT || (10 * 60 * 1000); // Default 10 min
+const BODY_LIMIT = process.env.BODY_PARSER_LIMIT || '1024mb';
+const REQUEST_TIMEOUT = process.env.REQUEST_TIMEOUT || (10 * 60 * 1000);
 const PORT = process.env.PORT;
+
+app.use(cors());
+app.use(contentSecurityPolicy);
+app.use(express.json({
+    limit: BODY_LIMIT,
+    timeout: REQUEST_TIMEOUT
+}));
+
+app.use(express.urlencoded({
+    limit: BODY_LIMIT,
+    extended: true,
+    timeout: REQUEST_TIMEOUT
+}));
+
+app.use('/', homeRoutes);
+app.use('/', registerRoutes);
+app.use('/', fileRoutes);
 
 app.listen(PORT, () => {
     console.log("servidor iniciado en el puerto:" + process.env.PORT);
--- app/routes/file.routes.js
+++ app/routes/file.routes.js
@@ -1,34 +1,10 @@
 const express = require('express');
 const auth = require('../middlewares/auth.middleware.js');
-const { registerUser, loginUser} = require('../controllers/register.controller.js');
-const { getFile,getFileInfo, createFile } = require('../controllers/file.controller.js');
-const { homeController } = require('../controllers/home.controller.js');
-const contentSecurityPolicy = require('../middlewares/contentSecurityPolicy.middleware.js');
-const cors = require('cors');
-var app = express();
+const { getFile, getFileInfo, createFile } = require('../controllers/file.controller.js');
+const router = express.Router();
 
-const SERVER_TIMEOUT = process.env.SERVER_TIMEOUT || (10 * 60 * 1000); // Default 10 min
-const BODY_LIMIT = process.env.BODY_PARSER_LIMIT || '1024mb';
-const REQUEST_TIMEOUT = process.env.REQUEST_TIMEOUT || (10 * 60 * 1000);
+router.post('/createFile', auth, createFile);
+router.get('/getFile/:id', getFile);
+router.get('/getFileInfo/:id', auth, getFileInfo);
 
-app.use(cors());
-app.use(contentSecurityPolicy);
-app.use(express.json({ 
-    limit: BODY_LIMIT,
-    timeout: REQUEST_TIMEOUT 
-}));
-
-app.use(express.urlencoded({ 
-    limit: BODY_LIMIT, 
-    extended: true,
-    timeout: REQUEST_TIMEOUT 
-}));
-
-app.post('/createFile', auth, createFile);
-app.get('/getFile/:id', getFile);
-app.get('/getFileInfo/:id',auth, getFileInfo);
-app.post('/register', registerUser);
-app.post('/login', loginUser);
-app.get('/',homeController);
-
-module.exports = app;
+module.exports = router;
